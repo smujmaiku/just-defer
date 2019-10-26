7dc605a031f3c2a2d76ed59193bf796bf4989532
Michael Szmadzinski <smujmaiku@gmail.com>
2019-10-26 15:44:35 -0400

    browser and requirejs support

--- src/just-defer.js
+++ src/just-defer.js
@@ -4,21 +4,29 @@
  * MIT Licensed
  */
 
-/**
- * Creates a defer for async usage
- * @returns {Object} {promise, resolve, reject, callback}
- */
-function justDefer() {
-	const defer = {};
-	defer.promise = new Promise((resolve, reject) => {
-		defer.resolve = resolve;
-		defer.reject = reject;
-	});
-	defer.callback = (err, res) => {
-		if (err) return defer.reject(err);
-		defer.resolve(res);
-	};
-	return defer;
-}
+;((root) => {
+	/**
+	 * Creates a defer for async usage
+	 * @returns {Object} {promise, resolve, reject, callback}
+	 */
+	function justDefer() {
+		const defer = {};
+		defer.promise = new Promise((resolve, reject) => {
+			defer.resolve = resolve;
+			defer.reject = reject;
+		});
+		defer.callback = (err, res) => {
+			if (err) return defer.reject(err);
+			defer.resolve(res);
+		};
+		return defer;
+	}
 
-exports = module.exports = justDefer;
+	if (typeof module !== 'undefined' && typeof exports === 'object') {
+		exports = module.exports = justDefer;
+	} else if (typeof define === 'function' && define.amd) {
+		define(() => justDefer);
+	} else {
+		root.justDefer = justDefer;
+	}
+})(this || (typeof window !== 'undefined' ? window : global));
